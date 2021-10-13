7912718925b92196382a903d100983f920cc3e5c
alfian_ezza <rianezza86@gmail.com>
2021-10-13 15:04:42 +0700

    Update

--- app/src/main/java/com/rianezza/fragmentbottomnavigation/MainActivity.java
+++ app/src/main/java/com/rianezza/fragmentbottomnavigation/MainActivity.java
@@ -1,14 +1,44 @@
 package com.rianezza.fragmentbottomnavigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_nav);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new ChatFragment()).commit();
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFrag = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_chat:
+                        selectedFrag = new ChatFragment();
+                        break;
+                    case R.id.nav_calls:
+                        selectedFrag = new CallFragment();
+                        break;
+                    case R.id.nav_story:
+                        selectedFrag = new StatusFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, selectedFrag).commit();
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/rianezza/fragmentbottomnavigation/StatusFragment.java
+++ app/src/main/java/com/rianezza/fragmentbottomnavigation/StatusFragment.java
@@ -7,14 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ChatFragment#newInstance} factory method to
+ * Use the {@link StatusFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ChatFragment extends Fragment {
+public class StatusFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +24,7 @@ public class ChatFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ChatFragment() {
+    public StatusFragment() {
         // Required empty public constructor
     }
 
@@ -35,11 +34,11 @@ public class ChatFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatFragment.
+     * @return A new instance of fragment StatusFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ChatFragment newInstance(String param1, String param2) {
-        ChatFragment fragment = new ChatFragment();
+    public static StatusFragment newInstance(String param1, String param2) {
+        StatusFragment fragment = new StatusFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,11 +58,7 @@ public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        return inflater.inflate(R.layout.fragment_status, container, false);
     }
 }
